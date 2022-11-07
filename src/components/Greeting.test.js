f0595fee195c47ee23093df4fa6d0ed156666d8f
Bruna Flor <brn.flor@gmail.com>
2022-11-07 16:42:27 +0000

    testing userEvents

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,44 @@
 import { render, screen } from "@testing-library/react";
 import { Greeting } from "./Greeting";
+import useEvent from "@testing-library/user-event";
 
-test("renders Hello World", () => {
-  //Arange
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders Hello World", () => {
+    //Arange
+    render(<Greeting />);
 
-  //Act
-  //...nothing
+    //Act
+    //...nothing
 
-  //Assert
-  const helloWorldElement = screen.getByText("Hello World");
-  expect(helloWorldElement).toBeInTheDocument();
+    //Assert
+    const helloWorldElement = screen.getByText("Hello World");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders It's good to see you if the button it's NOT clicked", () => {
+    //Arange
+    render(<Greeting />);
+
+    //Act
+    //...nothing
+
+    //Assert
+    const outputElement = screen.getByText("It's good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders Changed if the button was Clicked", () => {
+    //Arange
+    render(<Greeting />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    useEvent.click(buttonElement);
+
+    //Assert
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
 });
